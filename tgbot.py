1e1a7ea885ff9f80bd1a5dfe451bcf40750dbb83
Ireonx <78217105+Ireonx@users.noreply.github.com>
2022-10-09 14:51:12 +0300

    Add files via upload

---
+++ tgbot.py
@@ -0,0 +1,80 @@
+import telebot
+from telebot import types
+bot = telebot.TeleBot('5545425037:AAHX9cQ1hAGEPMPscnncuBAIRhY9e88FB-8')
+
+def mSend(text, mark = None):
+    global idfrom
+    if mark is not None:
+        bot.send_message(idfrom, text, reply_markup = mark)
+    else:
+        bot.send_message(idfrom, text)
+
+
+def send_rating(os, oe, hero = None):
+    global offset_start
+    global offset_end
+    rf = open("rating.txt", 'r')
+    if hero is not None:
+        for line in rf:
+            if f"Champion {hero}".lower() in line.lower():
+                mSend(line)
+                return 0
+        mSend("Чемпион не найден")
+        return 0
+    if os < 0:
+        offset_start = 0
+        offset_end = 10
+    if os > hero_count:
+        offset_start = int(hero_count/10)*10
+        offset_end = offset_start + 10
+    flines = rf.readlines()[offset_start:offset_end]
+    mSend("\n".join(flines))
+    rf.close()
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    global idfrom
+    global offset_start
+    global offset_end
+    mtext = message.text
+    idfrom = message.from_user.id
+    if mtext.lower() in ["/start", "привет"]:
+        mSend("Привет, чем я могу тебе помочь?", nav_markup)
+    elif mtext == "/help":
+        mSend("Напиши привет")
+    elif mtext == "NEXT":
+        offset_start += 10
+        offset_end += 10
+        send_rating(offset_start, offset_end)
+    elif mtext == "PREVIOUS":
+        offset_start -= 10
+        offset_end -= 10
+        send_rating(offset_start, offset_end)
+    elif len(mtext.split("rating ")) == 2:
+        cur_hero = mtext.split("rating ")[1]
+        send_rating(offset_start, offset_end, cur_hero)
+    elif mtext in ["/rating", "RATING"]:
+        send_rating(offset_start, offset_end)
+    else:
+        mSend("Я тебя не понимаю. Напиши /help.")
+
+def createMarkUp(names = []):
+    newMarkUp = types.ReplyKeyboardMarkup()
+    hlpbtn = types.KeyboardButton("HELP")
+    newMarkUp.row(hlpbtn)
+
+    for name in names:
+        newbtn = types.KeyboardButton(name)
+        newMarkUp.row(newbtn)
+
+    return newMarkUp
+
+
+idfrom = None
+offset_start = 0
+offset_end = 10
+nav_markup = createMarkUp(["NEXT", "PREVIOUS", "RATING"])
+hero_count = len(open("rating.txt", 'r').readlines())
+
+
+bot.polling(none_stop=True, interval=0)
--- topcards.py
+++ topcards.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
+import os
 
 df = pd.read_csv("allDecks.csv", index_col=0, header = None)
+
 
 df["other_champs"] = df.count(axis = 1) - 4
 df["deck_rating"] = df[4]/2000 + df[5]/5000*df[4] + df[6]*80*df["other_champs"]
@@ -13,13 +15,17 @@ df = df.drop([1, 2, 3], axis = 1)
 df1 = df["champs"].str.get_dummies("+")
 df = pd.concat([df, df1], axis = 1)
 
+print(df)
 rating = []
 
 for champ in df[df1.columns].columns:
     champ_data = df.loc[df[champ] == 1]
     l = (champ_data.values == 1).any(axis=0)
     cols = [champ_data.columns[idx] for idx in np.where(l == True)[0]]
+    print(cols)
     cols.remove(champ)
+    if 6 in cols:
+        cols.remove(6)
     if "other_champs" in cols:
         cols.remove("other_champs")
     # print(champ, sum(champ_data["deck_rating"]), cols)
@@ -28,5 +34,11 @@ for champ in df[df1.columns].columns:
     rating.append((champ, champ_value, neighbors))
 
 toprint = sorted(rating, key=lambda x: x[1], reverse=True)
+file_object = open('rating.txt', 'w')
 for i in range(1, len(toprint)+1):
-    print(f"{i}. Champion {toprint[i-1][0]} has a rating of {toprint[i-1][1]:.2f} and combines with {toprint[i-1][2]} \n")
+    strin = f"{i}. Champion {toprint[i-1][0]} has a rating of {toprint[i-1][1]:.2f} and combines with {toprint[i-1][2]} \n"
+    print(strin)
+    file_object.write(strin)
+file_object.close()
+
+os.remove("allDecks.csv")
